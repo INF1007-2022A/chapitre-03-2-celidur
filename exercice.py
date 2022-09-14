58ea933eedba98d7d1630b9404406b708ece36d2
celidur <gaetansjo@gmail.com>
2022-09-14 11:48:27 -0400

    exercice completed

--- exercice.py
+++ exercice.py
@@ -2,50 +2,51 @@
 
 
 def dissipated_power(voltage, resistance):
-	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+    return voltage ** 2 / resistance
+
 
 def orthogonal(v1, v2):
-	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+    return v1[0] * v2[0] + v1[1] * v2[1] == 0
+
 
 def average(values):
-	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
-	for v in values:
-		pass # La variable v contient une valeur de la liste.
+    # calcul the average of positive values in values
+    # return 0 if there is no positive value
+    total = 0
+    count = 0
+    for value in values:
+        if value >= 0:
+            total += value
+            count += 1
+    if count == 0:
+        return 0
+    return total / count
+
 
 def bills(value):
-	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
-	while value != 0:
-		if value >= 20:
-			pass
-		elif value >= 10:
-			pass
-		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
+    twenties, value = value // 20, value % 20
+    tens, value = value // 10, value % 10
+    fives, value = value // 5, value % 5
+    ones = value
+    return twenties, tens, fives, ones
 
-	return (twenties, tens, fives, twos, ones);
 
 def format_base(value, base, digit_letters):
-	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
-	# `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
-	result = ""
-	abs_value = abs(value)
-	while abs_value != 0:
-		pass
-	if value < 0:
-		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-		pass
-	return result
+    # Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
+    # `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
+    result = ""
+    abs_value = abs(value)
+    while abs_value != 0:
+        abs_value, digit = divmod(abs_value, base)
+        result += digit_letters[digit]
+    if value < 0:
+        result += "-"
+    return result[::-1]
 
 
 if __name__ == "__main__":
-	print(dissipated_power(69, 420))
-	print(orthogonal((1, 1), (-1, 1)))
-	print(average([1, 4, -2, 10]))
-	print(bills(137))
-	print(format_base(-42, 16, "0123456789ABCDEF"))
+    print(dissipated_power(69, 420))
+    print(orthogonal((1, 1), (-1, 1)))
+    print(average([1, 4, -2, 10]))
+    print(bills(137))
+    print(format_base(-42, 16, "0123456789ABCDEF"))
